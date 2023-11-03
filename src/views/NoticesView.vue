8e64fc93705607fbf55d3b2fa7f9a409ca262d2f
hayeooo <hayeon0709@daum.net>
2023-11-03 17:14:57 +0900

    style : 기본 header style 삭제

--- src/views/NoticesView.vue
+++ src/views/NoticesView.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import NoticesComponent from '@/components/NoticesComponent.vue'
 </script>
 <template>
   <main>
-    <!-- <RouterView /> -->
     <NoticesComponent />
   </main>
 </template>
