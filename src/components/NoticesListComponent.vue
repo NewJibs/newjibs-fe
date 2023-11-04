c84984da8b47b56aa9908c8a0aacb2d2dedf328e
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-05 01:34:28 +0900

    refactor : onmount하면 axios get 해오도록 변경

--- src/components/NoticesListComponent.vue
+++ src/components/NoticesListComponent.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
+import { ref, type Ref, onMounted } from 'vue'
 import { instance } from '@/api/axios';
 
-// interface Notice {
-//   title: string
-//   author: string
-//   date: string
-// }
+interface Notice {
+  title: string
+  author: string
+  date: string
+}
 
-// const data: Ref<Notice[]> = ref([])
+const data: Ref<Notice[]> = ref([])
 
 // const onClickGetData = () => {
 //   instance
@@ -21,6 +21,24 @@ import { instance } from '@/api/axios';
 //       console.error(res)
 //     })
 // }
+
+const fetchData = () => {
+  instance
+    .get('/notices', {})
+    .then((res) => {
+      console.log("axios get성공")
+      data.value = res.data
+    })
+    .catch((res) => {
+      console.error(res)
+    })
+}
+
+//페이지 로딩하자마자 데이터 fetch 해오기
+onMounted(() => {
+  fetchData()
+})
+
 </script>
 
 <template>
@@ -32,20 +50,20 @@ import { instance } from '@/api/axios';
           <th>제목</th>
           <th>작성자</th>
           <th>날짜</th>
-          <th >미리보기</th>
+          <!-- <th >미리보기</th> -->
         </tr>
       </thead>
       <tbody>
-        <!-- <tr v-for="notice in data" :key="notice.title">
+        <tr v-for="notice in data" :key="notice.title">
           <td>{{ notice.title }}</td>
           <td>{{ notice.author }}</td>
           <td>{{ notice.date }}</td>
-        </tr> -->
-        <tr>
-          <td>미리보기</td>
-          <td>미리보기</td>
-          <td>미리보기</td>
         </tr>
+        <!-- <tr>
+          <td>미리보기</td>
+          <td>미리보기</td>
+          <td>미리보기</td>
+        </tr> -->
       </tbody>
     </table>
   </div>
