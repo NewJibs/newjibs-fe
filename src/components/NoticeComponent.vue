51fccd8feff339579350ab9a0d86497a97a216e9
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-05 01:57:13 +0900

    feat : notice 개별 페이지 구현

--- src/components/NoticeComponent.vue
+++ src/components/NoticeComponent.vue
@@ -1,28 +1,43 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
+import { ref, type Ref, onMounted } from 'vue'
 import { instance } from '@/api/axios';
+import { useRoute, useRouter } from 'vue-router';
 
-// interface Notice {
-//   title: string
-//   author: string
-//   date: string
-// }
+interface Notice {
+  title: string
+  author: string
+  date: string
+}
 
-// const data: Ref<Notice[]> = ref([])
+const data = ref<Notice | null>(null)
+const router = useRouter()
+const route = useRoute()
 
-// const onClickGetData = () => {
-//   instance
-//     .get('/notices', {})
-//     .then((res) => {
-//       console.log('axios get 성공')
-//       data.value = res.data
-//     })
-//     .catch((res) => {
-//       console.error(res)
-//     })
-// }
+//noticeId별 정보 fetch 해오기
+const loadNotice = (noticeId: number) => {
+    instance
+        .get(`/notices/${noticeId}`)
+        .then((res) => {
+            console.log("axios get 성공")
+            data.value = res.data
+        })
+        .catch((res) => {
+            console.error(res)
+        })
+}
+
+//페이지 로딩하자마자 데이터 fetch 해오기
+onMounted(() => {
+    const noticeId = Number(route.params.noticeId)
+    if(!isNaN(noticeId)) {
+        loadNotice(noticeId)
+    } else {
+        //noticeId가 유효하지 않을 때 처리
+        router.push({path: '/'})
+    }
+})
+
 </script>
-
 <template>
   <div class="container">
     <!-- <button @click="onClickGetData()">버튼입</button> -->
@@ -36,16 +51,16 @@ import { instance } from '@/api/axios';
         </tr>
       </thead>
       <tbody>
-        <!-- <tr v-for="notice in data" :key="notice.title">
+        <tr v-for="notice in data" :key="notice.title">
           <td>{{ notice.title }}</td>
           <td>{{ notice.author }}</td>
           <td>{{ notice.date }}</td>
-        </tr> -->
-        <tr>
-          <td>미리보기</td>
-          <td>미리보기</td>
-          <td>미리보기</td>
         </tr>
+        <!-- <tr>
+          <td>미리보기</td>
+          <td>미리보기</td>
+          <td>미리보기</td>
+        </tr> -->
       </tbody>
     </table>
   </div>
