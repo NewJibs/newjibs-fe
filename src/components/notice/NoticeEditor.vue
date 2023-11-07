03d376d9f8961ae0c733f811e5346238c253ad79
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-07 17:00:32 +0900

    fix : editor vue3 composition api 문법으로 변경

--- src/components/notice/NoticeEditor.vue
+++ src/components/notice/NoticeEditor.vue
@@ -1,46 +1,40 @@
-<script setup lang="ts">
-import { onMounted, type Ref, ref, watch, onUnmounted } from 'vue';
-
-import Editor from '@toast-ui/editor';
-import 'codemirror/lib/codemirror.css'; // Editor's Dependency Style
-import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
-import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
-
-const props = defineProps({
-    modelValue: {
-        type: String,
-        required: false,
-        default: '',
-    }
-})
-
-const emit = defineEmits(['update:modelValue'])
-const editor = ref()
-const editorValid = ref()
-const testHtml = ref()
-
-//페이지가 마운트될 때 editor 생성
-onMounted(() => {
-    editorValid.value = new Editor({
-        el: editor.value,
-        height: '500px',
-        initialEditType: 'wysiwyg',
-        events: {
-            change: () => emit('update:modelValue', editorValid.value.getMarkdown()),
-        }
-    })
-})
-
-//작성한 내용 불러와서 html 적용
-const testValid = (e: any) => {
-    testHtml.value = editorValid.value.getHTML()
-}
-</script>
-
 <template>
+  <div>
     <div ref="editor"></div>
-    <div v-html="testHtml.value"></div>
+    <div v-html="testHtml"></div>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import Editor from '@toast-ui/editor';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+const editor = ref(null);
+const editorInstance = ref(null);
+const testHtml = ref('');
+
+onMounted(() => {
+  editorInstance.value = new Editor({
+    el: editor.value,
+    height: '500px',
+    initialEditType: 'wysiwyg',
+    events: {
+      change: () => emit('update:modelValue', editorInstance.value.getMarkdown()),
+    },
+  });
+});
+
+const testValid = () => {
+  testHtml.value = editorInstance.value.getHTML();
+};
+
+onUnmounted(() => {
+  if (editorInstance.value) {
+    editorInstance.value.destroy();
+  }
+});
+</script>
 
 <style scoped></style>
