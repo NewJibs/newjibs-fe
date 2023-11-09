ff0de9dbb916236ac40fcd782462304a4c7fd09a
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-10 05:47:45 +0900

    fix : 삭제 매개변수 삭제

--- src/components/notice/NoticeListItem.vue
+++ src/components/notice/NoticeListItem.vue
@@ -29,18 +29,41 @@ const loadNotice = (noticeId: number) => {
 }
 
 //noticeId별 delete -> 홈화면으로 라우터 설정
-const deletePost = async (noticeId: number) => {
-  try {
-    await instance.delete(`/notices/${noticeId}`);
-    router.push({ name: 'notices' }); // 삭제 후 /notices로 리디렉트
-  } catch (error) {
-    console.error('게시물 삭제 실패', error);
+const deletePost = () => {
+  instance
+    .delete(`/notices/${noticeId}`,{
+      withCredentials: true
+    })
+    .then(() => {
+      router.push({ name: 'notices' })})
+    .catch((error) => {
+      console.error('게시물 삭제 실패', error);
+    });
+}
+
+const updatePost = async () => {
+  if (!isNaN(noticeId) && data.value) {
+    const updatedData = { ...data.value }; // 복사해서 수정할 데이터를 생성
+    updatedData.title = '새로운 제목'; // 원하는 필드를 수정
+    updatedData.author = '새로운 작성자'; // 원하는 필드를 수정
+
+    // 서버에 수정 요청 보내기
+    try {
+      const response = await instance.put(`/notices/${noticeId}`, updatedData, {
+        withCredentials: true,
+      });
+
+      // 수정 성공한 경우
+      console.log('게시물 수정 성공', response.data);
+
+      // 수정한 데이터를 다시 불러오기 (선택적)
+      loadNotice(noticeId);
+    } catch (error) {
+      // 수정 실패한 경우
+      console.error('게시물 수정 실패', error);
+    }
   }
-}
-
-const updatePost = async (noticeId: number) => {
-
-}
+};
 
 //페이지 로딩하자마자 데이터 fetch 해오기
 onMounted(() => {
