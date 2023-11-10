1c3c4bef614519fcfcfc853b12c7ca915d6d1c43
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-10 17:49:47 +0900

    #2 feat : navbar router link 구현

--- src/components/common/TheNavBar.vue
+++ src/components/common/TheNavBar.vue
@@ -2,9 +2,13 @@
   <v-app ref="app">
     <v-app-bar>
       <v-tabs v-model="tab" background-color="primary">
-        <router-link :to="{ name: 'home' }"><v-tab to="">newjibs</v-tab></router-link>
+        <router-link :to="{ name: 'home' }" class="routerLink">
+          <v-tab>newjibs</v-tab>
+        </router-link>
         <v-tab to="/map">지도</v-tab>
-        <router-link :to="{ name: 'notices' }"><v-tab to="">공지사항</v-tab></router-link>
+        <router-link :to="{ name: 'notices' }" class="routerLink">
+          <v-tab>공지사항</v-tab>
+        </router-link>
         <v-tab to="/news">뉴스</v-tab>
       </v-tabs>
     </v-app-bar>
@@ -25,4 +29,14 @@ import { ref } from 'vue'
 const tab = ref(null)
 </script>
 
-<style scoped></style>
+<style>
+.routerLink {
+  color: black;
+}
+.routerLink:visited {
+  color: blue;
+}
+.routerLink:hover {
+  color: blue;
+}
+</style>
