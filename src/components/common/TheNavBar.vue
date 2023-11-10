9f0ca339f2951752d95f27f8e2ee1538877fc471
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-10 15:41:59 +0900

    #2 style : vuetify로 변경

--- src/components/common/TheNavBar.vue
+++ src/components/common/TheNavBar.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-    <div>
-        여기는 네브바
-    </div>
+  <div>여기는 네브바</div>
 </template>
 <style scoped></style>
--- src/components/notice/NoticeList.vue
+++ src/components/notice/NoticeList.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref, type Ref, onMounted, computed, unref } from 'vue'
-import type { TableProps, TableColumnType } from 'ant-design-vue';
-import { instance } from '@/util/http-common';
-import { useRouter } from 'vue-router';
+import type { TableProps, TableColumnType } from 'ant-design-vue'
+import { instance } from '@/util/http-common'
+import { useRouter } from 'vue-router'
 
 interface Notice {
   noticeId: number
@@ -17,7 +17,7 @@ const fetchData = () => {
   instance
     .get('/notices', {})
     .then((res) => {
-      console.log("axios get성공")
+      console.log('axios get성공')
       console.log(res)
       data.value = res.data
     })
@@ -32,75 +32,54 @@ onMounted(() => {
 })
 
 //and design용
-const columns: TableColumnType<Notice>[] = [
-  {
-    title: '제목',
-    dataIndex: 'title',
-  },
-  {
-    title: '작성자',
-    dataIndex: 'author',
-  },
-  {
-    title: '작성일시',
-    dataIndex: 'date',
-  },
-];
-
-// const rowSelection: TableProps['rowSelection'] = {
-//   getCheckboxProps: (record: any) => ({
-//     title: record.title,
-//   }),
-// };
+// const columns: TableColumnType<Notice>[] = [
+//   {
+//     title: '제목',
+//     dataIndex: 'title'
+//   },
+//   {
+//     title: '작성자',
+//     dataIndex: 'author'
+//   },
+//   {
+//     title: '작성일시',
+//     dataIndex: 'date'
+//   }
+// ]
 
 //클릭하면 noticeId에 해당하는 페이지로 이동
-const router = useRouter();
+const router = useRouter()
 
 const viewNotice = (noticeId: number) => {
   //공지사항의 noticeId를 동적 라우트 매개변수로 넘겨 해당 페이지로 이동
-  router.push({name: 'notice', params: {noticeId: noticeId.toString()}})
+  router.push({ name: 'notice', params: { noticeId: noticeId.toString() } })
 }
 
 const postNotice = () => {
-  router.push({name: 'notice-post'})
+  router.push({ name: 'notice-post' })
 }
-
 </script>
 
 <template>
-  <div>
-    <a-table  :columns="columns" :data-source="data">
-      <template #bodyCell="{ record, text }">
-          <a @click="viewNotice(record.noticeId)">{{ text }}</a>
-      </template>
-    </a-table>
-    <div class="container">
-      <a-button type="primary" @click="postNotice">
-        게시글 작성
-      </a-button>
-    </div>
-  </div>
-  <!-- <div class="container">
-    <table>
+  <div class="container">
+    <v-table>
       <thead>
         <tr>
-          <th>번호</th>
-          <th>제목</th>
-          <th>작성자</th>
-          <th>날짜</th>
+          <th class="text-center">제목</th>
+          <th class="text-center">작성자</th>
+          <th class="text-center">작성일</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="notice in data" :key="notice.noticeId" @click="viewNotice(notice.noticeId)">
-          <td>{{ notice.noticeId }}</td>
+        <tr v-for="notice in data" :key="notice.noticeId" class="text-center">
           <td>{{ notice.title }}</td>
           <td>{{ notice.author }}</td>
           <td>{{ notice.date }}</td>
         </tr>
       </tbody>
-    </table>
-    <router-link to="/notices/post">post버튼</router-link>
-  </div> -->
+    </v-table>
+    <v-btn type="submit" @click="postNotice"> 게시글 작성 </v-btn>
+  </div>
 </template>
 
 <style scoped></style>
