6f69072df3f1dcc6f170a617f0e02205854313c0
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-12 18:01:33 +0900

    #2 navbar size 변화

--- src/components/common/TheNavBar.vue
+++ src/components/common/TheNavBar.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app ref="app">
-    <v-app-bar>
-      <v-tabs v-model="tab" background-color="primary">
+    <v-app-bar class="app-bar">
+      <v-tabs v-model="tab" background-color="primary" class="app-bar-tab">
         <router-link :to="{ name: 'home' }" class="routerLink">
-          <v-tab>newjibs</v-tab>
+          <v-tab class="app-tab">newjibs</v-tab>
         </router-link>
-        <v-tab to="/map">지도</v-tab>
+        <v-tab class="app-tab" to="/map">지도</v-tab>
         <router-link :to="{ name: 'notices' }" class="routerLink">
-          <v-tab>공지사항</v-tab>
+          <v-tab class="app-tab">공지사항</v-tab>
         </router-link>
-        <v-tab to="/news">뉴스</v-tab>
+        <v-tab class="app-tab" to="/news">뉴스</v-tab>
       </v-tabs>
     </v-app-bar>
 
@@ -29,8 +29,24 @@ import { ref } from 'vue'
 const tab = ref(null)
 </script>
 
-<style>
+<style lang="scss">
 .routerLink {
   color: black;
+}
+
+.app-bar {
+  height: 6rem;
+  display: flex;
+  align-items: center;
+}
+
+.app-bar-tab {
+  display: flex;
+  align-items: center;
+  height: 6rem;
+}
+
+.app-tab {
+  font-size: 1.3rem;
 }
 </style>
