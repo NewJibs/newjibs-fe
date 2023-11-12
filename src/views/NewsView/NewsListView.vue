3f91c95b9f94d0f5233eefa76f52e48f6768f7ba
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-12 23:13:24 +0900

    #3 feat : newslistview 구현

---
+++ src/views/NewsView/NewsListView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NewsDetail from '@/components/news/NewsList.vue'
+</script>
+<template>
+  <main>
+    <NewsDetail />
+  </main>
+</template>
+<style scoped></style>
