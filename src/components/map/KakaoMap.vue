b5eef92d402979192e1dde4029a8a5b2fa9e04d1
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-17 12:18:25 +0900

    #8 style : navbar랑 지도랑 크기 맞추기

--- src/components/map/KakaoMap.vue
+++ src/components/map/KakaoMap.vue
@@ -86,6 +86,6 @@ const deleteMarkers = () => {
 <style>
 #map {
   width: 100%;
-  height: 100vh;
+  height: 94vh;
 }
 </style>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view></router-view>
+  <div style="height: fit-content">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style scoped></style>
