7664bc2389a8ca7e801a8e848f77b210df77dcbf
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-17 12:08:28 +0900

    feat : ranking component, view 생성

--- src/views/CommonView/ErrorView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import TheError from '@/components/common/TheError.vue'
-</script>
-<template>
-  <TheError />
-</template>
-<style scoped></style>
---
+++ src/views/RankingView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>랭킹</div>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
