74d93abd97f6e1fd6704d98ca6dd391c0be2f98a
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-18 23:05:12 +0900

    #8 fix : 클릭해도 잘 안뜨는 에러 해결

--- src/components/map/KakaoMap.vue
+++ src/components/map/KakaoMap.vue
@@ -71,10 +71,8 @@ const placesSearchCB = (data, status, pagination) => {
 }
 
 const displayPlaces = (places) => {
-  const order = document.getElementById(currCategory).getAttribute('data-order')
-
   for (let i = 0; i < places.length; i++) {
-    const marker = addMarker(new kakao.maps.LatLng(places[i].y, places[i].x), order)
+    const marker = addMarker(new kakao.maps.LatLng(places[i].y, places[i].x), places[i].order)
 
     ;(function (marker, place) {
       kakao.maps.event.addListener(marker, 'click', function () {
@@ -155,9 +153,9 @@ const addCategoryClickEvent = () => {
   }
 }
 
-const onClickCategory = () => {
-  const id = this.id
-  const className = this.className
+const onClickCategory = (e) => {
+  const id = e.currentTarget.id
+  const className = e.currentTarget.className
 
   placeOverlay.setMap(null)
 
@@ -167,7 +165,7 @@ const onClickCategory = () => {
     removeMarker()
   } else {
     currCategory = id
-    changeCategoryClass(this)
+    changeCategoryClass(e.currentTarget)
     searchPlaces()
   }
 }
@@ -184,37 +182,38 @@ const changeCategoryClass = (el) => {
     el.className = 'on'
   }
 }
-
-const changeSelector = (e) => {
-  currCategory = e.target.value
-  searchPlaces()
-}
 </script>
 
 <template>
   <div class="map_wrap">
     <div id="map"></div>
-    <div class="dropdown" style="position: absolute; z-index: 9999999; top: 4vh">
-      <button
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        주변 시설 선택
-      </button>
-      <select class="dropdown-menu" id="category" @click="changeSelector">
-        <option class="dropdown-item" id="MT1" value="MT1" data-order="0">대형마트</option>
-        <option class="dropdown-item" id="CS2" value="CS2" data-order="1">편의점</option>
-        <option class="dropdown-item" id="PS3" value="PS3" data-order="2">유치원</option>
-        <option class="dropdown-item" id="SC4" value="SC4" data-order="3">학교</option>
-        <option class="dropdown-item" id="CT1" value="CT1" data-order="4">문화시설</option>
-        <option class="dropdown-item" id="HP8" value="HP8" data-order="5">병원</option>
-        <option class="dropdown-item" id="FD6" value="FD6" data-order="6">음식점</option>
-        <option class="dropdown-item" id="CE7" value="CE7" data-order="7">카페</option>
-      </select>
-    </div>
-  </div>
+    <ul id="category">
+        <li id="BK9" data-order="0"> 
+            <span class="category_bg bank"></span>
+            은행
+        </li>       
+        <li id="MT1" data-order="1"> 
+            <span class="category_bg mart"></span>
+            마트
+        </li>  
+        <li id="PM9" data-order="2"> 
+            <span class="category_bg pharmacy"></span>
+            약국
+        </li>  
+        <li id="OL7" data-order="3"> 
+            <span class="category_bg oil"></span>
+            주유소
+        </li>  
+        <li id="CE7" data-order="4"> 
+            <span class="category_bg cafe"></span>
+            카페
+        </li>  
+        <li id="CS2" data-order="5"> 
+            <span class="category_bg store"></span>
+            편의점
+        </li>      
+    </ul>
+</div>
 </template>
 
 <style>
@@ -222,4 +221,30 @@ const changeSelector = (e) => {
   width: 100%;
   height: 94vh;
 }
+.map_wrap, .map_wrap * {margin:0; padding:0;font-family:'Malgun Gothic',dotum,'돋움',sans-serif;font-size:12px;}
+.map_wrap {position:relative;width:100%;height:350px;}
+#category {position:absolute;top:10px;left:10px;border-radius: 5px; border:1px solid #909090;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);background: #fff;overflow: hidden;z-index: 2;}
+#category li {float:left;list-style: none;width:50px;border-right:1px solid #acacac;padding:6px 0;text-align: center; cursor: pointer;}
+#category li.on {background: #eee;}
+#category li:hover {background: #ffe6e6;border-left:1px solid #acacac;margin-left: -1px;}
+#category li:last-child{margin-right:0;border-right:0;}
+#category li span {display: block;margin:0 auto 3px;width:27px;height: 28px;}
+#category li .category_bg {background:url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png) no-repeat;}
+#category li .bank {background-position: -10px 0;}
+#category li .mart {background-position: -10px -36px;}
+#category li .pharmacy {background-position: -10px -72px;}
+#category li .oil {background-position: -10px -108px;}
+#category li .cafe {background-position: -10px -144px;}
+#category li .store {background-position: -10px -180px;}
+#category li.on .category_bg {background-position-x:-46px;}
+.placeinfo_wrap {position:absolute;bottom:28px;left:-150px;width:300px;}
+.placeinfo {position:relative;width:100%;border-radius:6px;border: 1px solid #ccc;border-bottom:2px solid #ddd;padding-bottom: 10px;background: #fff;}
+.placeinfo:nth-of-type(n) {border:0; box-shadow:0px 1px 2px #888;}
+.placeinfo_wrap .after {content:'';position:relative;margin-left:-12px;left:50%;width:22px;height:12px;background:url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
+.placeinfo a, .placeinfo a:hover, .placeinfo a:active{color:#fff;text-decoration: none;}
+.placeinfo a, .placeinfo span {display: block;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;}
+.placeinfo span {margin:5px 5px 0 5px;cursor: default;font-size:13px;}
+.placeinfo .title {font-weight: bold; font-size:14px;border-radius: 6px 6px 0 0;margin: -1px -1px 0 -1px;padding:10px; color: #fff;background: #d95050;background: #d95050 url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png) no-repeat right 14px center;}
+.placeinfo .tel {color:#0f7833;}
+.placeinfo .jibun {color:#999;font-size:11px;margin-top:0;}
 </style>
