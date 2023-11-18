3be578e2faaf72c974a2b2414b0a1a4982d14118
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-18 22:43:31 +0900

    #8 feat : 카테고리별 정보 마커 표시하기

--- src/components/map/KakaoMap.vue
+++ src/components/map/KakaoMap.vue
@@ -1,104 +1,193 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue'
 
-let map
-const positions = ref([])
-const markers = ref([])
+let placeOverlay,
+  contentNode,
+  markers = []
+let currCategory = ''
+let mapContainer, mapOption, map
 const ps = ref()
 
-// const props = defineProps({ stations: Array, selectStation: Object })
-
-//이렇게 map import 해오는거다 강조!
 onMounted(() => {
   if (window.kakao && window.kakao.maps) {
     initMap()
   } else {
     const script = document.createElement('script')
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${
-      //autoload=false 꼭 해주기
       import.meta.env.VITE_KAKAO_MAP_SERVICE_KEY
     }&libraries=services,clusterer`
-    // global kakao 지우지 말기 - 카카오는 전역변수라고 eslint한테 알려주는 것
-    /* global kakao */
+
     script.onload = () => kakao.maps.load(() => initMap())
     document.head.appendChild(script)
   }
 })
 
-//초기 카카오맵의 위치
 const initMap = () => {
+  mapContainer = document.getElementById('map')
+  mapOption = {
+    center: new kakao.maps.LatLng(37.566826, 126.9786567),
+    level: 5
+  }
+  map = new kakao.maps.Map(mapContainer, mapOption)
   ps.value = new kakao.maps.services.Places(map)
 
-  const container = document.getElementById('map')
-  const options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3 //확대 3레벨
-  }
-  map = new kakao.maps.Map(container, options)
+  contentNode = document.createElement('div')
+  contentNode.className = 'placeinfo_wrap'
 
-  // loadMarkers();
+  placeOverlay = new kakao.maps.CustomOverlay({ zIndex: 1, content: contentNode })
+
+  kakao.maps.event.addListener(map, 'idle', searchPlaces)
+
+  addCategoryClickEvent()
 }
 
-const loadMarkers = () => {
-  // 현재 표시되어있는 marker들이 있다면 map에 등록된 marker를 제거한다.
-  deleteMarkers()
+const addEventHandle = (target, type, callback) => {
+  if (target.addEventListener) {
+    target.addEventListener(type, callback)
+  } else {
+    target.attachEvent('on' + type, callback)
+  }
+}
 
-  // 마커 이미지를 생성합니다
-  //   const imgSrc = require("@/assets/map/markerStar.png");
-  // 마커 이미지의 이미지 크기 입니다
-  //   const imgSize = new kakao.maps.Size(24, 35);
-  //   const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
+const searchPlaces = () => {
+  if (!currCategory) {
+    return
+  }
 
-  // 마커를 생성합니다
-  markers.value = []
-  positions.value.forEach((position) => {
-    const marker = new kakao.maps.Marker({
-      map: map, // 마커를 표시할 지도
-      position: position.latlng, // 마커를 표시할 위치
-      title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨.
-      clickable: true // // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-      // image: markerImage, // 마커의 이미지
-    })
-    markers.value.push(marker)
+  placeOverlay.setMap(null)
+  removeMarker()
+
+  ps.value.categorySearch(currCategory, placesSearchCB, { useMapBounds: true })
+}
+
+const placesSearchCB = (data, status, pagination) => {
+  if (status === kakao.maps.services.Status.OK) {
+    displayPlaces(data)
+  } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+    // 검색결과가 없는경우 해야할 처리가 있다면 이곳에 작성해 주세요
+  } else if (status === kakao.maps.services.Status.ERROR) {
+    // 에러로 인해 검색결과가 나오지 않은 경우 해야할 처리가 있다면 이곳에 작성해 주세요
+  }
+}
+
+const displayPlaces = (places) => {
+  const order = document.getElementById(currCategory).getAttribute('data-order')
+
+  for (let i = 0; i < places.length; i++) {
+    const marker = addMarker(new kakao.maps.LatLng(places[i].y, places[i].x), order)
+
+    ;(function (marker, place) {
+      kakao.maps.event.addListener(marker, 'click', function () {
+        displayPlaceInfo(place)
+      })
+    })(marker, places[i])
+  }
+}
+
+const addMarker = (position, order) => {
+  const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png'
+  const imageSize = new kakao.maps.Size(27, 28)
+  const imgOptions = {
+    spriteSize: new kakao.maps.Size(72, 208),
+    spriteOrigin: new kakao.maps.Point(46, order * 36),
+    offset: new kakao.maps.Point(11, 28)
+  }
+  const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions)
+  const marker = new kakao.maps.Marker({
+    position: position,
+    image: markerImage
   })
 
-  // 4. 지도를 이동시켜주기
-  // 배열.reduce( (누적값, 현재값, 인덱스, 요소)=>{ return 결과값}, 초기값);
-  // 마커가 다 보이도록 지도의 확대레벨 자동 세팅
-  const bounds = positions.value.reduce(
-    (bounds, position) => bounds.extend(position.latlng),
-    new kakao.maps.LatLngBounds()
-  )
+  marker.setMap(map)
+  markers.push(marker)
 
-  map.setBounds(bounds)
+  return marker
 }
 
-//배열의 크기가 0보다 크면 배열을 지워라
-const deleteMarkers = () => {
-  if (markers.value.length > 0) {
-    markers.value.forEach((marker) => marker.setMap(null))
+const removeMarker = () => {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(null)
+  }
+  markers = []
+}
+
+const displayPlaceInfo = (place) => {
+  let content =
+    '<div class="placeinfo">' +
+    '   <a class="title" href="' +
+    place.place_url +
+    '" target="_blank" title="' +
+    place.place_name +
+    '">' +
+    place.place_name +
+    '</a>'
+
+  if (place.road_address_name) {
+    content +=
+      '    <span title="' +
+      place.road_address_name +
+      '">' +
+      place.road_address_name +
+      '</span>' +
+      '  <span class="jibun" title="' +
+      place.address_name +
+      '">(지번 : ' +
+      place.address_name +
+      ')</span>'
+  } else {
+    content += '    <span title="' + place.address_name + '">' + place.address_name + '</span>'
+  }
+
+  content +=
+    '    <span class="tel">' + place.phone + '</span>' + '</div>' + '<div class="after"></div>'
+
+  contentNode.innerHTML = content
+  placeOverlay.setPosition(new kakao.maps.LatLng(place.y, place.x))
+  placeOverlay.setMap(map)
+}
+
+const addCategoryClickEvent = () => {
+  const category = document.getElementById('category')
+  const children = category.children
+
+  for (let i = 0; i < children.length; i++) {
+    children[i].onclick = onClickCategory
   }
 }
 
-//드롭다운의 카테고리 선택하는 함수
+const onClickCategory = () => {
+  const id = this.id
+  const className = this.className
+
+  placeOverlay.setMap(null)
+
+  if (className === 'on') {
+    currCategory = ''
+    changeCategoryClass()
+    removeMarker()
+  } else {
+    currCategory = id
+    changeCategoryClass(this)
+    searchPlaces()
+  }
+}
+
+const changeCategoryClass = (el) => {
+  const category = document.getElementById('category')
+  const children = category.children
+
+  for (let i = 0; i < children.length; i++) {
+    children[i].className = ''
+  }
+
+  if (el) {
+    el.className = 'on'
+  }
+}
+
 const changeSelector = (e) => {
-  console.log(ps.value)
-  ps.value.categorySearch(
-    e.target.value,
-    (data, status, pagination) => {
-      if (status === kakao.maps.services.Status.OK) {
-        positions.value = data.map((item) => ({
-          latlng: new kakao.maps.LatLng(item.y, item.x),
-          title: item.place_name
-        }))
-        loadMarkers()
-      } else {
-        console.log('데이터를 가져오는데 실패했습니다.')
-      }
-    },
-    { useMapBounds: true }
-  )
-  //이거 결과값이 배열로 오는데 이거를 positions.value.push 한다음에 console.log 부분에 loadMarkers해주면됨
+  currCategory = e.target.value
+  searchPlaces()
 }
 </script>
 
@@ -128,7 +217,6 @@ const changeSelector = (e) => {
   </div>
 </template>
 
-<!-- 카카오맵은 전역으로 설치되어 있기 때문에 scoped말고 그냥 style -->
 <style>
 #map {
   width: 100%;
