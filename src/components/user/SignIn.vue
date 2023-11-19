af5984d483bca1f16eb251ecd39aa819dd3a042b
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-19 16:42:27 +0900

    feat : login form 만들기

--- src/components/user/SignIn.vue
+++ src/components/user/SignIn.vue
@@ -1,28 +1,115 @@
-<script setup></script>
+<script setup>
+// @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
+</script>
 
 <template>
-  <div class="container" style="z-index: 100;">
+  <div class="container" style="z-index: 100">
     <div class="forms-container">
       <div class="signin-signup">
         <form action="" class="sign-in-form">
           <h2 class="title">Login</h2>
           <div class="input-field">
             <i class="fas fa-user"></i>
-            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Name" />
           </div>
           <div class="input-field">
             <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Password">
+            <input type="password" placeholder="Password" />
           </div>
-          <input type="submit" value="login" class="btn solid">
-          <p class="social-text"></p>
+          <input type="submit" value="login" class="btn solid" />
         </form>
       </div>
     </div>
-    <div class="panels-container">
-    
-    </div>
+    <div class="panels-container"></div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fff;
+  overflow: hidden;
+}
+
+.forms-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.title {
+  font-size: 2.2rem;
+  color: #444;
+  margin-bottom: 10px;
+}
+
+.input-field {
+  max-width: 380px;
+  width: 100%;
+  height: 55px;
+  background-color: #f0f0f0;
+  margin: 10px 0;
+  border-radius: 55px;
+  display: grid;
+  grid-template-columns: 15% 85%;
+  padding: 0 0.4rem;
+}
+
+.input-field i {
+  text-align: center;
+  line-height: 55px;
+  color: #acacac;
+  font-size: 1.1rem;
+}
+
+.input-field input {
+  background: none;
+  outline: none;
+  border: none;
+  line-height: 1;
+  font-weight: 600;
+  font-size: 1.1rem;
+  color: #333;
+}
+
+.input-field input::placeholder {
+  color: #aaa;
+  font-weight: 500;
+}
+
+.btn {
+  width: 150px;
+  height: 49px;
+  border: none;
+  outline: none;
+  border-radius: 49px;
+  cursor: pointer;
+  background-color: #5995fd;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 10px 0;
+  transition: 0.5s;
+}
+
+.btn:hover {
+  background-color: #4d84e2;
+}
+</style>
