53b706e3272615d92c193bdc8b4368e3db220076
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-20 11:25:50 +0900

    style : 설정 폰트 적용

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,7 @@ import TheNavBar from './components/common/TheNavBar.vue'
 @font-face {
   font-family: 'NeoDunggeunmoPro';
   src: url('@/fonts/NeoDunggeunmoPro-Regular.ttf') format('truetype');
-}
-@font-face {
-  font-family: 'NeoDunggeunmoPro';
-  src: url('@/fonts/NeoDunggeunmoPro-Regular.woff') format('truetype');
+  src: url('@/fonts/NeoDunggeunmoPro-Regular.woff') format('woff');
+
 }
 </style>
