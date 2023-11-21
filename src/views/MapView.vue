5f7ad25cee163458a3d59ac6635150918169a2b4
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-22 01:11:57 +0900

    feat : 지도페이지 로그인 안되어있을 시 접근제한

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user-store'
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+
+const userStore = useUserStore()
+const router = useRouter()
+
+//로그인 안되었을 시 접근제한
+onMounted(() => {
+  if (!userStore.accessToken) {
+    alert('로그인 후 접근해주세요.')
+    router.push({ name: 'user-login' })
+  }
+})
+</script>
 
 <template>
-  <div style="height: fit-content">
+  <div>
     <router-view></router-view>
   </div>
 </template>
