e560c1438d5c507204ff0a9f3d66a0a870c2d5a4
smarfy99 <jjwzzy4643@gmail.com>
2023-11-22 04:03:21 +0900

    [Hybrid -> dev] 지금까지 한거 (#23)
    * feat : mypage 페이지 생성 및 라우팅
    
    * style : navbar 화이트로 변경
    
    * style : 아파트 마커 변경
    
    * chore : 필요없는 코드 삭제
    
    * fix : netlify에서 새로고침하면 page not found에러 뜨는 현상 fixed
    
    * feat : 회원가입하면 로그인페이지로 이동
    
    * fix : 로그아웃하면 로그인페이지로 라우팅
    
    * feat : 지도페이지 로그인 안되어있을 시 접근제한
    
    * feat : 지도 페이지 접근시 로그인페이지로 라우팅 + 모달
    
    * fix : news detail 개행문자 -> <br>로 변경
    
    * style : news carousel card 조정
    
    * style : 제목 글씨 변경
    
    * feat : news list carousel 3개씩
    
    * feat : news list 하단에 card 형태로 setting
    
    * feat : mount이전 beforemount때 데이터 패칭해오기
    
    * feat : cluster 클릭하면 확대되는 기능 구현
    
    * feat : 클러스터 클릭하면 일정 지연시간 후에 지도확대
    
    * feat : map - keyword 검색 부분 위치 조정 완료
    
    * feat : news에 lazy loading 넣기

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -15,6 +15,7 @@ interface News {
   publishDateTime: string
 }
 
+const chunkData: Ref<News[][]> = ref([])
 const data: Ref<News[]> = ref([])
 
 const fetchData = async () => {
@@ -28,17 +29,47 @@ const fetchData = async () => {
 }
 
 onBeforeMount(async () => {
-  console.log('before mount')
-  // await fetchData()
-  console.log('data fetched')
+  // console.log('before mount')
+  //전체 데이터
+  const articles = await fetchData()
+  data.value = reactive(articles.data)
+
+  //청크된 데이터
+  const chunkedData = chunkArray(articles.data, 3)
+  chunkData.value = reactive(chunkedData)
+  // console.log('data fetched')
 })
 
-onMounted(async () => {
-  const articles = await fetchData()
-  console.log(articles)
-  data.value = reactive(articles.data)
-  console.log('Mounted')
-})
+// onMounted(async () => {
+//   const articles = await fetchData()
+//   // console.log(articles)
+//   data.value = reactive(articles.data)
+//   // console.log('Mounted')
+// })
+
+// onMounted(async () => {
+//   //전체 데이터
+//   const articles = await fetchData()
+//   data.value = reactive(articles.data)
+
+//   //청크된 데이터
+//   const chunkedData = chunkArray(articles.data, 3)
+//   chunkData.value = reactive(chunkedData)
+// })
+
+//3개씩 자른 chunk array
+const chunkArray = (array: any[], chunkSize: number) => {
+  let index = 0
+  let arrayLength = array.length
+  let tempArray = []
+
+  for (index = 0; index < arrayLength; index += chunkSize) {
+    let chunk = array.slice(index, index + chunkSize)
+    tempArray.push(chunk)
+  }
+
+  return tempArray
+}
 
 const router = useRouter()
 
@@ -51,8 +82,8 @@ const viewNews = (articleId: string) => {
 </script>
 
 <template>
-  <div class="back">
-    <h1>부동산 뉴스</h1>
+  <!-- <div class="back">
+    <h1 style="font-family: 'NeoDunggeunmoPro'">부동산 뉴스</h1>
     <div class="notice-detail-container">
       <v-carousel
         height="500"
@@ -68,60 +99,125 @@ const viewNews = (articleId: string) => {
           :key="news.articleId"
           @click.prevent="viewNews(news.articleId)"
         >
-          <v-sheet height="100%">
-            <v-container>
-              <v-card class="card-fixed-size">
-                <v-card-item><img class="card-img" :src="news.thumbnail" /></v-card-item>
-                <v-card-title class="card-title">{{ news.title }}</v-card-title>
-                <v-card-subtitle
-                  >{{ news.pressCorporationName }} |
-                  {{ new Date(news.publishDateTime).toLocaleDateString() }}</v-card-subtitle
-                >
-                <v-card-text>{{ news.summaryContent }}...</v-card-text>
-              </v-card>
-            </v-container>
-          </v-sheet>
+          <v-card class="mx-auto" max-width="340">
+            <v-img :src="news.thumbnail" height="200px" cover></v-img>
+            <v-card-title>{{ news.title }}</v-card-title>
+            <v-card-subtitle
+              >{{ news.pressCorporationName }} |
+              {{ new Date(news.publishDateTime).toLocaleDateString() }}</v-card-subtitle
+            >
+            <v-card-text>{{ news.summaryContent }}...</v-card-text>
+          </v-card>
         </v-carousel-item>
       </v-carousel>
     </div>
+  </div> -->
+  <div class="back">
+    <h1 style="font-family: 'NeoDunggeunmoPro'">부동산 뉴스</h1>
+    <div class="notice-detail">
+      <v-carousel
+        height="500"
+        hide-delimiters
+        cover
+        show-arrows="hover"
+        cycle
+        interval="5000"
+        hover
+        transition="slide-x-transition"
+      >
+        <v-carousel-item
+          v-for="(news, index) in chunkData"
+          :key="index"
+          @click.prevent="viewNews(news[index].articleId)"
+        >
+          <v-row no-gutters>
+            <v-col cols="4" v-for="item in news" :key="item.articleId">
+              <v-card class="mx-auto news-card" max-width="330">
+                <v-img :src="item.thumbnail" height="12.5rem" cover></v-img>
+                <v-card-title>{{ item.title }}</v-card-title>
+                <v-card-subtitle
+                  >{{ item.pressCorporationName }} |
+                  {{ new Date(item.publishDateTime).toLocaleDateString() }}</v-card-subtitle
+                >
+                <v-card-text>{{ item.summaryContent }}...</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </div>
+
+    <!-- 뉴스 보드형식 -->
+    <v-row align="center" justify="center">
+      <v-col cols="auto" v-for="(news, index) in data" :key="index">
+        <v-hover v-slot="{ isHovering }">
+          <v-lazy :min-height="200" :options="{ threshold: 0.2 }" transition="fade-transition">
+            <v-card
+              @click.prevent="viewNews(news.articleId)"
+              class="mx-auto"
+              max-width="500"
+              :title="news.title"
+              :subtitle="news.pressCorporationName"
+              :elevation="isHovering ? 12 : 2"
+            >
+              <template v-slot:prepend>
+                <img :src="news.thumbnail" style="width: 150px; height: 100px" />
+              </template>
+              <v-card-text>{{ news.summaryContent }}...</v-card-text>
+            </v-card>
+          </v-lazy>
+        </v-hover>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <style scoped lang="scss">
 .back {
   background-image: url('@/assets/news-back.png');
-  background-size: cover;
+  background-size: cover; /* 이미지가 컨테이너에 꽉 차도록 설정 */
+  background-attachment: fixed; /* 이미지 위치를 고정 */
+  background-position: center center; /* 이미지를 중앙에 위치시킴 */
+  background-repeat: no-repeat; /* 이미지가 반복되지 않도록 설정 */
+  width: 100%; /* 너비를 화면 전체로 설정 */
+  height: 100vh; /* 높이를 뷰포트 높이로 설정 */
 }
 
-.card-fixed-size {
-  width: 300px;
-  height: 430px;
+.news-card {
+  height: 390px;
 }
 
-.card-img {
-  object-fit: cover;
-  width: 100%;
-  height: 200px;
+.v-window.v-item-group.theme--light.v-window--horizontal
+  .v-window-item.v-window-item--prev-leave-to,
+.v-window.v-item-group.theme--light.v-window--horizontal
+  .v-window-item.v-window-item--next-leave-to {
+  opacity: 0;
+  transform: translateX(0);
+  transition:
+    opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1),
+    transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);
 }
 
-/* .v-window__next {
-  display: none;
+.v-window.v-item-group.theme--light.v-window--horizontal .v-window-item.v-window-item--next-enter,
+.v-window.v-item-group.theme--light.v-window--horizontal .v-window-item.v-window-item--prev-enter {
+  opacity: 0;
+  transform: translateX(0);
 }
 
-.v-window__prev {
-  display: none;
+.v-window.v-item-group.theme--light.v-window--horizontal
+  .v-window-item.v-window-item--next-leave-active,
+.v-window.v-item-group.theme--light.v-window--horizontal
+  .v-window-item.v-window-item--prev-leave-active {
+  transition:
+    opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1),
+    transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);
 }
 
-.v-window__container:hover {
-  .v-window__prev {
-    display: block;
-  }
-  .v-window__next {
-    display: block;
-  }
-} */
-
-.news-card:hover {
-  cursor: pointer;
+.v-window.v-item-group.theme--light.v-window--horizontal
+  .v-window-item.v-window-item--next-enter-to,
+.v-window.v-item-group.theme--light.v-window--horizontal
+  .v-window-item.v-window-item--prev-enter-to {
+  opacity: 1;
+  transform: translateX(0);
 }
 </style>
