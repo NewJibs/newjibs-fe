2cfede3ab8d5b9ff6889e867fdb75da270c2dcb3
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-22 01:22:43 +0900

    feat : 지도 페이지 접근시 로그인페이지로 라우팅 + 모달

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user-store'
 import { useRouter } from 'vue-router'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const userStore = useUserStore()
 const router = useRouter()
+const dialog = ref(false) //모달
 
 //로그인 안되었을 시 접근제한
 onMounted(() => {
   if (!userStore.accessToken) {
-    alert('로그인 후 접근해주세요.')
-    router.push({ name: 'user-login' })
+    dialog.value = true
   }
 })
+
+//모달을 닫았을 때 라우팅
+const closeDialogAndNavigate = () => {
+  dialog.value = false
+  router.push({ name: 'user-login' })
+}
 </script>
 
 <template>
   <div>
     <router-view></router-view>
+
+    <v-dialog v-model="dialog" width="auto">
+      <v-card>
+        <v-card-text> 로그인 후 접근해주세요. </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="closeDialogAndNavigate">확인</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
