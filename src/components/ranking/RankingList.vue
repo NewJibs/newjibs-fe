e4ed16f8cc69cfbd5429a02918124e115d104d91
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-11-24 00:52:17 +0900

    feat : x icon 위치

--- src/components/ranking/RankingList.vue
+++ src/components/ranking/RankingList.vue
@@ -6,7 +6,91 @@ import { useHomeStore } from '@/stores/home-store'
 let data = reactive({})
 const dialog = ref(true) //결과 모달창
 const homeStore = useHomeStore() //pinia store 사용
-let finalData = homeStore.resultData //pinia store에서 데이터 가져오기
+// let finalData = homeStore.resultData //pinia store에서 데이터 가져오기
+const finalData = {
+  sumPriceGap: 47500,
+  sumPercentageGap: 4.75,
+  results: [
+    {
+      priceChange: {
+        beforeDealAmount: 66000,
+        beforeDealYear: 2020,
+        beforeDealMonth: 2,
+        beforeDealDay: 23,
+        afterDealAmount: 92000,
+        afterDealYear: 2022,
+        afterDealMonth: 4,
+        afterDealDay: 11,
+        priceGap: 26000,
+        percentageGap: 39.39
+      },
+      houseInfo: {
+        aptCode: 11110000000018,
+        no: 111102002000038,
+        area: '59.9426',
+        sidoName: '서울특별시',
+        gugunName: '종로구',
+        dongName: '숭인동',
+        buildYear: 2009,
+        roadName: '숭인동길',
+        roadNameBonBun: '00021',
+        roadNameBubun: '00000',
+        roadNameSeq: '01',
+        roadNameBasementCode: '0',
+        roadNameCode: '4100204',
+        dong: '숭인동',
+        bonbun: '0766',
+        bubun: '0000',
+        sigunguCode: '11110',
+        eubmyundongCode: '17500',
+        landCode: '1',
+        apartmentName: '종로청계힐스테이트',
+        jibun: '766',
+        lng: '127.020959405051',
+        lat: '37.5757847596495'
+      }
+    },
+    {
+      priceChange: {
+        beforeDealAmount: 100000,
+        beforeDealYear: 2020,
+        beforeDealMonth: 1,
+        beforeDealDay: 13,
+        afterDealAmount: 121500,
+        afterDealYear: 2022,
+        afterDealMonth: 4,
+        afterDealDay: 13,
+        priceGap: 21500,
+        percentageGap: 21.5
+      },
+      houseInfo: {
+        aptCode: 11110000000018,
+        no: 111102001000044,
+        area: '114.7153',
+        sidoName: '서울특별시',
+        gugunName: '종로구',
+        dongName: '숭인동',
+        buildYear: 2009,
+        roadName: '숭인동길',
+        roadNameBonBun: '00021',
+        roadNameBubun: '00000',
+        roadNameSeq: '01',
+        roadNameBasementCode: '0',
+        roadNameCode: '4100204',
+        dong: '숭인동',
+        bonbun: '0766',
+        bubun: '0000',
+        sigunguCode: '11110',
+        eubmyundongCode: '17500',
+        landCode: '1',
+        apartmentName: '종로청계힐스테이트',
+        jibun: '766',
+        lng: '127.020959405051',
+        lat: '37.5757847596495'
+      }
+    }
+  ]
+}
 
 //랭킹정보 가져오기
 const fetchRanking = async () => {
@@ -17,24 +101,53 @@ const fetchRanking = async () => {
 
 onMounted(() => {
   fetchRanking()
-  console.log(data)
 })
 </script>
 
 <template>
   <div class="all_wrap">
     <!-- 모달창 -->
-    <!-- <v-row justify="center"> -->
-    <v-dialog v-model="dialog" width="800" transition="dialog-top-transition">
+    <v-dialog v-model="dialog" width="800" transition="dialog-expand-transition">
+      <!-- <div
+        style="
+          display: flex;
+          justify-content: center;
+          align-content: center;
+          width: 30rem;
+          height: 12rem;
+          background-color: red;
+        "
+      > -->
       <h1
         style="font-size: 3rem; font-family: 'NeoDunggeunmoPro'; color: yellow; text-align: center"
       >
         부동산 거래 결과
       </h1>
-      <v-card>
-        <v-btn icon dark @click="dialog = false">
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
+      <!-- </div> -->
+      <v-card style="display: flex">
+        <v-row>
+          <v-col cols="11">
+            <!-- ... -->
+            <v-list lines="two">
+              <!-- ... -->
+            </v-list>
+          </v-col>
+          <v-col cols="1">
+            <v-btn icon width="25" height="25" variant="text" style="align-self: flex-start">
+              <font-awesome-icon
+                style="margin-left: 0.5rem; margin-top: 0.5rem"
+                icon="xmark"
+                @click="dialog = false"
+              ></font-awesome-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <!-- 총 수익 -->
+        <div style="display: flex">
+          <div style="display: flex; flex-direction: column"></div>
+          <div style="display: flex; flex-direction: column"></div>
+        </div>
+
         <v-list lines="two">
           <v-list-item v-for="result in finalData?.results" :key="result.houseInfo.no">
             <v-list-item-content>
@@ -56,11 +169,20 @@ onMounted(() => {
         </v-list>
       </v-card>
     </v-dialog>
-    <!-- </v-row> -->
 
     <div class="board_wrap">
-      <h1 style="font-size: 4rem; font-family: 'NeoDunggeunmoPro'; color: yellow">Ranking</h1>
-      <v-list lines="three" style="font-family: 'NeoDunggeunmoPro'">
+      <div style="display: flex; align-items: center; justify-content: center">
+        <h1 style="font-size: 4rem; color: yellow; margin-right: 1rem">Ranking</h1>
+        <v-btn
+          color="primary"
+          style="font-size: 1.3rem"
+          width="150"
+          height="40"
+          @click="dialog = true"
+          >결과 보기</v-btn
+        >
+      </div>
+      <v-list lines="three">
         <v-list-item v-for="(user, rank) in data" :key="rank">
           <v-list-item-content>
             <div
@@ -99,6 +221,7 @@ onMounted(() => {
   height: calc(100vh - 3.9rem);
   background-image: url('@/assets/ranking-back.png');
   background-size: cover;
+  font-family: 'NeoDunggeunmoPro';
 }
 
 .board_wrap {
